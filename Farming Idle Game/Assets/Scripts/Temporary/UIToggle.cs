305864c6c586be679ac975511269864618c57478
lholloway912 <lholloway912@gmail.com>
2026-03-08 21:10:47 -0400

    Created Blockout and Temp UI

---
+++ Farming Idle Game/Assets/Scripts/Temporary/UIToggle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToggle : MonoBehaviour
+{
+    private GameObject seedShopUI;
+    void Start()
+    {
+        seedShopUI = GameObject.Find("SeedShopUI");
+        seedShopUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            seedShopUI.SetActive(!seedShopUI.activeSelf);
+        }
+    }
+
+}
