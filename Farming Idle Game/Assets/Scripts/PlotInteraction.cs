64849bb2730f24c82925b07d369c6b487efa003d
ElleDeRoin <ellederoin@gmail.com>
2026-03-14 08:07:18 -0500

    Updated PlotPrefab

--- Farming Idle Game/Assets/Scripts/PlotInteraction.cs
+++ Farming Idle Game/Assets/Scripts/PlotInteraction.cs
@@ -4,75 +4,139 @@ using UnityEngine;
 
 public class PlotInteraction : MonoBehaviour, IInteractable
 {
-    [SerializeField] private GameObject PlantPrefab;
+    [Header("Prefabs")]
+    [SerializeField] private GameObject plantPrefab;
     [SerializeField] private GameObject dirtMoundPrefab;
-    private bool hasPlant = false;
-    private PlantCycle plantCycle;
-    UiManager uimanager;
 
+    [Header("Grid Settings")]
+    [SerializeField] private int gridSize = 3;
+    [SerializeField] private float plantSpacing = 1.0f;
+
+    private bool hasPlant = false;
     private bool playerInRange = false;
-    private GameObject Plant;
-    private GameObject Dirt;
+
+    private List<GameObject> plants = new List<GameObject>();
+    private List<GameObject> dirtMounds = new List<GameObject>();
+    private List<PlantCycle> plantCycles = new List<PlantCycle>();
+
     private MoneyManager moneyManager;
 
     void Start()
     {
-
         moneyManager = FindObjectOfType<MoneyManager>();
-
-
     }
+
     void Update()
     {
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             Interact();
         }
-        
     }
 
     public void Interact()
     {
-        Debug.Log("Interacted with the plot!");
         if (!hasPlant)
         {
-            Dirt = Instantiate(dirtMoundPrefab, transform.position + new Vector3(0, 0.1f, 0), Quaternion.identity);
-            Plant = Instantiate(PlantPrefab, transform.position + new Vector3(0, 0.4f, 0), Quaternion.identity);
-            plantCycle = Plant.GetComponent<PlantCycle>();
-            hasPlant = true;
+            PlantGrid();
         }
-        else if (plantCycle != null && plantCycle.isGrowing == false)
+        else if (AllPlantsGrown())
         {
             Harvest();
-        } 
+        }
         else
         {
-            Debug.Log("Plant is still growing!");
+            Debug.Log("Plants are still growing!");
         }
     }
 
-    private void Harvest()
+    void PlantGrid()
     {
+        int half = gridSize / 2;
+
+        for (int x = -half; x <= half; x++)
+        {
+            for (int z = -half; z <= half; z++)
+            {
+                Vector3 offset = new Vector3(x * plantSpacing, 0, z * plantSpacing);
+
+                
+                GameObject dirt = Instantiate(
+                    dirtMoundPrefab,
+                    transform.position + offset + new Vector3(0, 0.1f, 0),
+                    Quaternion.identity
+                );
+
+                dirt.transform.parent = transform;
+                dirtMounds.Add(dirt);
+
+                
+                GameObject newPlant = Instantiate(
+                    plantPrefab,
+                    transform.position + offset + new Vector3(0, 0.4f, 0),
+                    Quaternion.identity
+                );
+
+                newPlant.transform.parent = transform;
+                plants.Add(newPlant);
+
+                PlantCycle cycle = newPlant.GetComponent<PlantCycle>();
+                if (cycle != null)
+                {
+                    plantCycles.Add(cycle);
+                }
+            }
+        }
+
+        hasPlant = true;
+    }
+
+    bool AllPlantsGrown()
+    {
+        foreach (PlantCycle plant in plantCycles)
+        {
+            if (plant.isGrowing)
+                return false;
+        }
+
+        return true;
+    }
+
+    void Harvest()
+    {
+        moneyManager.AddMoney(5 * plants.Count);
+
+        foreach (GameObject plant in plants)
+        {
+            if (plant != null)
+                Destroy(plant);
+        }
+
+        foreach (GameObject dirt in dirtMounds)
+        {
+            if (dirt != null)
+                Destroy(dirt);
+        }
+
+        plants.Clear();
+        dirtMounds.Clear();
+        plantCycles.Clear();
+
         hasPlant = false;
-        moneyManager.AddMoney(5);
 
-        if (Plant != null)
-            Destroy(Plant);
-
-        if (Dirt != null)
-            Destroy(Dirt);
-
-        Debug.Log("Plant has been harvested!");
+        Debug.Log("Plants harvested!");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        playerInRange = true;
+        if (other.CompareTag("Player"))
+            playerInRange = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        playerInRange = false;
+        if (other.CompareTag("Player"))
+            playerInRange = false;
     }
 
     public string GetInteractPrompt()
@@ -80,10 +144,12 @@ public class PlotInteraction : MonoBehaviour, IInteractable
         if (!hasPlant)
         {
             return "Press [E] to plant!";
-        } else if ((plantCycle != null && !plantCycle.isGrowing))
+        }
+        else if (AllPlantsGrown())
         {
             return "Press [E] to harvest!";
-        } else
+        }
+        else
         {
             return "Growing...";
         }
