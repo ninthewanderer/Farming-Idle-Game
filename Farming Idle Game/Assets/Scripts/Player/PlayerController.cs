5d0001f178cf570ab19c2cafa6d89fcdbff00b11
Chandler Guzman <ninthewander@gmail.com>
2026-03-31 14:39:09 -0400

    Update PlayerController.cs
    more commenting, just deleted old code because we can look back on old commits if we need it tbh

--- Farming Idle Game/Assets/Scripts/Player/PlayerController.cs
+++ Farming Idle Game/Assets/Scripts/Player/PlayerController.cs
@@ -3,16 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* Commented out a whole bunch of the old code because it's not working with the new camera and it's too complicated
- for me to debug TBH. */
+// Removed the old code because it's not working with the new camera and it's too complicated for me to debug TBH.
 
 public class PlayerController : MonoBehaviour
 {
-    // private PlayerControls controls;
-    // private Vector3 movement;
-    // [SerializeField] private float _turnSmoothTime = 0.1f;
-    // private float _turnSmoothVelocity;
-    
     private ResourceManager resourceManager;
     private CharacterController playerController;
     [SerializeField] private float _moveSpeed = 6f;
@@ -30,49 +24,31 @@ public class PlayerController : MonoBehaviour
         // Locks cursor (can be changed later)
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        
-        // controls = new PlayerControls();
-        // controls.Player.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
-        // controls.Player.Move.canceled += ctx => movement = Vector3.zero;
     }
 
     private void Update()
     {
-        // Old code for movement & rotation.
-        
-        // Variables needed for movement.
-        // float horizontal = Input.GetAxisRaw("Horizontal");
-        // float vertical = Input.GetAxisRaw("Vertical");
-        // Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        //
-        // // Smoothly turns the player in the direction they're moving in.
-        // if (direction.magnitude >= 0.1f)
-        // {
-        //     float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        //     float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, 
-        //                   _turnSmoothTime);
-        //     transform.rotation = Quaternion.Euler(0f, angle, 0f);
-        //     
-        //     Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        //     playerController.Move(moveDir.normalized * _moveSpeed * Time.deltaTime);
-        // }
-        
+        // Used to determine if there is input and for target direction calculation.
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Vertical");
         
+        // Actual values used for movement. 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         if (vertical < 0) vertical *= -1;
         if (horizontal < 0) horizontal *= -1;
         float move = vertical + horizontal;
         
+        // Determines the forward direction of the player and uses it to determine movement direction.
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         moveDirection = forward * move;
         moveDirection.Normalize();
         moveDirection *= _moveSpeed;
         
+        // Calculates the direction the player will be moving in based on the camera's position.
         UpdateTargetDirection();
         
+        // Rotates the player in the direction the camera is facing.
         if (input != Vector2.zero && targetDirection.magnitude >= 0.1f)
         {
             Vector3 lookDirection = targetDirection.normalized;
@@ -88,13 +64,14 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), turnSpeed * Time.deltaTime);
         }
         
+        // Moves the player as long as there is input.
         if (input != Vector2.zero)
         {
-            // moveDirection.y = moveDirectionY;
             playerController.Move(moveDirection * Time.deltaTime);
         }
     }
 
+    // Updates the direction of the player relative to the camera. 
     private void UpdateTargetDirection()
     {
         var forward = Camera.main.transform.TransformDirection(Vector3.forward);
@@ -103,18 +80,6 @@ public class PlayerController : MonoBehaviour
         var right = Camera.main.transform.TransformDirection(Vector3.right);
         targetDirection = input.x * right + input.y * forward;
     }
-
-    // Activates Input Actions when the object is enabled.
-    // private void OnEnable()
-    // {
-    //     controls.Player.Enable();
-    // }
-    //
-    // // Deactivates Input Actions when the object is disabled.
-    // private void OnDisable()
-    // {
-    //     controls.Player.Disable();
-    // }
 
     // Get and set so shop can upgrade player speed
     public void SetMoveSpeed(float newSpeed)
